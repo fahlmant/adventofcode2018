ec12d333a80bd112313f7413b79eae7c9d5e96ee
Taylor Fahlman <tfahlman@redhat.com>
2018-12-03 10:15:17 -0500

    My solution

--- day1/p2.py
+++ day1/p2.py
@@ -1,5 +1,14 @@
-from itertools import accumulate, cycle
+import itertools
 with open('input') as f:
         lines = f.readlines()
-numseen = set()
-print(next(i for i in accumulate(cycle(lines)) if i in numseen or numseen.add(i)))
+
+numsum = 0
+numseen = {0}
+
+for num in itertools.cycle(lines):
+    numsum += int(num)
+    if numsum in numseen:
+        print(numsum)
+        break
+    numseen.add(numseen)
+
